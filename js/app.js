fad50b51d18e96ebb68652c5253f7698fde3e14e
ericstretton <eric.stretton@gmail.com>
2022-04-06 16:26:33 -0600

    Update post and delete post working.

--- js/app.js
+++ js/app.js
@@ -13,9 +13,32 @@ function createPost(){
     }).then(postSuccess).catch(postFail);
 }
 
+function updatePost(){
+    axios.request({
+        method : "PATCH",
+        url : "https://jsonplaceholder.typicode.com/posts/101",
+        headers : {
+            'Content-type': 'application/json',
+        },
+        data : {
+            body : document.getElementById('update').value
+        }
+    }).then(updateSuccess).catch(updateFailure);
+}
+
+function deletePost(){
+    axios.request({
+        method : "DELETE",
+        url : "https://jsonplaceholder.typicode.com/posts/101",
+        headers : {
+            'Content-type': 'application/json',
+        },
+    }).then(deleteSuccess).catch(deleteFail);
+}
+
 function postSuccess(response){
     console.log(response.data);
-    Cookies.set('sessionToken', response.data.id);
+    Cookies.set('postID', response.data.id);
     document.getElementById('postResult').innerText="Comment Submitted.";
 }
 
@@ -24,4 +47,30 @@ function postFail(error){
     document.getElementById('postResult').innerText="Error, Comment has Failed to Post!";
 }
 
+function updateSuccess(response){
+    console.log(response);
+    console.log(response.data.body);
+    document.getElementById('postResult').innerText="Update Successful!";
+}
+
+function updateFailure(error){
+    console.log(error);
+    document.getElementById('postResult').innerText="Error, your attempt to Update your comment has failed!";
+}
+
+function deleteSuccess(response){
+    console.log(response);
+    document.getElementById('postResult').innerText="Delete of comment was Successful";
+    Cookies.remove('postID', response.data.id);
+}
+
+function deleteFail(error){
+    console.log(error);
+    document.getElementById('postResult').innerText="Error, the post has not been deleted";
+}
+
 document.getElementById('postSubmit').addEventListener('click', createPost);
+
+document.getElementById('updateSubmit').addEventListener('click', updatePost);
+
+document.getElementById('deleteSubmit').addEventListener('click', deletePost);
